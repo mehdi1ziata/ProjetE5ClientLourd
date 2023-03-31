2abad1c779ca65db2d101c946a8e0843a1961497
mehdi <mehdi@LAPTOP-VQ3G47N3>
2023-03-31 11:09:45 +0200

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+
+namespace PPEProjet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+            string connectionString = "Server=127.0.0.1;Database=bibliotheque;Uid=root;Pwd='';";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "SELECT * FROM utilisateurs WHERE login=@username";
+                    command.Parameters.AddWithValue("@username", username);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string hashedPassword = Hashage.GenerateSHA256String(password).ToLower();
+                            if (hashedPassword == reader["mdp"].ToString())
+                            {
+                                MessageBox.Show("Connexion réussie !");
+                                int niveauAdmin = 2;
+                                if (niveauAdmin == int.Parse(reader["niveau"].ToString()))
+                                {
+                                    fenetreAdmin uneFenetreAdmin = new fenetreAdmin();
+                                    uneFenetreAdmin.Show();
+                                    uneFenetreAdmin.textBlockLabelAdmin.Content = uneFenetreAdmin.textBlockLabelAdmin.Content + " " + reader["prenom"];
+                                }
+                                else
+                                {
+                                    fenetreOperateur uneFenetreOperateur = new fenetreOperateur();
+                                    uneFenetreOperateur.Show();
+                                    uneFenetreOperateur.textBlockLabelOp.Text = uneFenetreOperateur.textBlockLabelOp.Text + " " + reader["prenom"];
+                                }
+                            }else MessageBox.Show("Nom d'utilisateur ou mot de passe incorrect !");
+                        }
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ fenetreAdmin.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace PPEProjet
+{
+    /// <summary>
+    /// Logique d'interaction pour fenetreAdmin.xaml
+    /// </summary>
+    public partial class fenetreAdmin : Window
+    {
+        public fenetreAdmin()
+        {
+            InitializeComponent();
+            string connectionString = "Server=127.0.0.1;Database=bibliotheque;Uid=root;Pwd='';";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = "SELECT * FROM utilisateurs;";
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+
+                            List<Utilisateurs> listUtilisateurs = new List<Utilisateurs>();
+                            Utilisateurs utilisateur = new Utilisateurs(reader["prenom"].ToString(), reader["nom"].ToString(), reader["prenom"].ToString(), reader["prenom"].ToString());
+                            listUtilisateurs.Add(utilisateur);
+                            
+                        }
+                    }
+
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }   
+        }
+
+        public class Utilisateurs
+        {
+            private string prenom;
+            private string nom;
+            private string mdp;
+            private string login;
+
+            public Utilisateurs(string lePrenom, string leNom, string leMdp, string leLogin)
+            {
+                prenom = lePrenom;
+                nom = leNom;
+                mdp = leMdp;
+                login = leLogin;
+            }
+
+            public string Prenom { get { return prenom; } set { prenom = value; } }
+
+            public string Nom { get { return nom; } set { nom = value; } }
+
+            public string Mdp { get { return mdp; } set { mdp = value; } }
+
+            public string Login { get { return login; } set { login = value; } }
+
+        }
+    }
+}
+
